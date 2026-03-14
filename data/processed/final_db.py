fb321d3bff3676e38c4eb532e5b1f93f45a40b9f
Iago Rivadulla <iagorivadullaact@gmail.com>
2026-03-14 19:27:10 +0100

    changing date format

--- data/processed/final_db.py
+++ data/processed/final_db.py
@@ -4,11 +4,15 @@ import sqlalchemy as db
 def final_db(engine):
     #engine = db.create_engine('sqlite:///../raw/data.db')
 
-    stocks = pd.read_sql('SELECT * FROM stocks_processed', con=engine)
-    fed = pd.read_sql('SELECT * FROM macro_processed', con=engine)
-    events = pd.read_sql('SELECT * FROM events_processed', con=engine)
-    event_day = pd.read_sql('SELECT * FROM events_countdown', con=engine)
+    stocks = pd.read_sql('SELECT * FROM stocks_processed', engine)
+    fed = pd.read_sql('SELECT * FROM macro_processed', engine)
+    events = pd.read_sql('SELECT * FROM events_processed', engine)
+    event_day = pd.read_sql('SELECT * FROM events_countdown', engine)
 
-    df = pd.concat([events, fed, stocks, event_day], axis=1, sort=True).dropna(how='any')
+    for df_temp in (stocks, fed, events, event_day):
+        df_temp['date'] = pd.to_datetime(df_temp['date'])
+        df_temp.set_index('date', inplace=True)
+
+    df = pd.concat([events, fed, stocks, event_day], axis=1).dropna()
 
     return df
